32c346f966457a5dd5d391d5752321aa9c348fe6
Liu Yun <145887014+stoneforeverly@users.noreply.github.com>
2025-04-27 00:00:02 +0800

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -8,9 +8,7 @@ terraform {
 
 # Provider configuration
 provider "aws" {
-  region = "ap-southeast-2"  
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region = "ap-southeast-2"
 }
 
 # Create a security group in the default VPC
